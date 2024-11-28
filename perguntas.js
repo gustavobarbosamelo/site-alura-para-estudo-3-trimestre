9c0649b1587319c3000591f52d70a2c38e67086b
gustavobarbosamelo <141241206+gustavobarbosamelo@users.noreply.github.com>
2024-11-27 22:31:22 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é javascript?',
+    'O Javascript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Coreia do Sul?',
+    'A capital da Coreia do Sul é Seul'
+)
+
+criaCartao(
+    'Programação',
+    'O que são dicionários?',
+    'Um dicionário é uma estrutura de dados que armazena pares de chave e valor, o que permite associar dados de maneira mais organizada e eficiente.'
+)
+
+criaCartao(
+    'Linguagem Coreana',
+    'Como se diz Boa Tarde em Coreano ?',
+    'Em coreano, "Boa tarde" se diz 안녕하세요 (pronuncia-se "Annyeonghaseyo").'
+)
